be627e202c5b9631eae255b560c378a8beb270d9
Kingsley Yung <kingsley@kkoyung.dev>
2023-08-16 21:33:20 +0800

    Change border of documentation hover window

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -73,3 +73,9 @@ null_ls.setup {
     code_actions.gitsigns,
   },
 }
+
+-- UI customization
+-- Change border of documentation hover window, See https://github.com/neovim/neovim/pull/13998.
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+  border = "rounded",
+})
